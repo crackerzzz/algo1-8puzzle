7649bcc7b514df92e466cd2b7e2d508da621937d
crackerzzz <shreejwal.shrestha@gmail.com>
2019-01-08 08:59:49 +1100

    Implemented Board. Added samples.

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
 
+import edu.princeton.cs.algs4.In;
+
 public class Board {
+	private static final String SPACE = " ";
+	private static final String NEWLINE = "\n";
 	private final int[][] blocks;
 	private final int n;
 
@@ -28,7 +32,27 @@ public class Board {
 	 * @return number of blocks out of place
 	 */
 	public int hamming() {
-		return -1;
+		int counter = 1;
+		int outOfPlace = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (blocks[i][j] != counter++ && blocks[i][j] > 0) {
+					outOfPlace++;
+				}
+			}
+		}
+		return outOfPlace;
+	}
+
+	private Cell findEmptyBlock() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (blocks[i][j] == 0) {
+					new Cell(i, j);
+				}
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -54,8 +78,21 @@ public class Board {
 		return null;
 	}
 
-	// does this board equal y?
-	public boolean equals(Object y) {
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Board other = (Board) obj;
+		if (n != other.n) {
+			return false;
+		}
+		if (!Arrays.deepEquals(blocks, other.blocks)) {
+			return false;
+		}
 		return true;
 	}
 
@@ -64,11 +101,57 @@ public class Board {
 		return null;
 	}
 
-	// string representation of this board (in the output format specified below)
+	private static String padLeft(String s, int n) {
+		return String.format("%1$" + n + "s", s);
+	}
+
+	/**
+	 * string representation of this board (in the output format specified below)
+	 */
 	public String toString() {
-		return "";
+		final StringBuilder builder = new StringBuilder();
+		int prefix = 1;
+		if (n >= 10) {
+			prefix = 2;
+		} else if (n >= 100) {
+			prefix = 3;
+		}
+		for (int[] rows : blocks) {
+			for (int col : rows) {
+				builder.append(padLeft(col + "", prefix));
+				builder.append(SPACE);
+			}
+			builder.append(NEWLINE);
+		}
+		return builder.toString();
+	}
+
+	private static class Cell {
+		public final int i;
+		public final int j;
+
+		public Cell(int i, int j) {
+			super();
+			this.i = i;
+			this.j = j;
+		}
 	}
 
 	public static void main(String[] args) {
+		// read in the board specified in the filename
+		In in = new In("./8puzzle/puzzle00.txt");
+		int n = in.readInt();
+		int[][] tiles = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				tiles[i][j] = in.readInt();
+			}
+		}
+
+		// solve the slider puzzle
+		Board initial = new Board(tiles);
+		System.out.println(initial);
+		System.out.println(initial.hamming());
 	}
+
 }
