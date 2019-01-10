52656f1700bc4729c7e03a4080ac44f2c2343c8d
crackerzzz <shreejwal.shrestha@gmail.com>
2019-01-10 11:18:18 +1100

    Implemented Solver.

--- src/Solver.java
+++ src/Solver.java
@@ -1,11 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
+	private final MinPQ<SearchNode> pq;
+	private int moves;
+	private final List<Board> boards = new ArrayList<>();
+
 	/**
 	 * find a solution to the initial board (using the A* algorithm)
 	 * 
 	 * @param initial
 	 */
 	public Solver(Board initial) {
+		if (initial == null) {
+			throw new IllegalArgumentException();
+		}
+		pq = new MinPQ<>();
+		pq.insert(new SearchNode(initial));
+		do {
+			final SearchNode min = pq.delMin();
+			boards.add(min.current);
+			if (min.current.isGoal()) {
+				break;
+			}
+			final int finalMoves = ++moves;
+			StreamSupport.stream(min.current.neighbors()
+				.spliterator(), false)
+				.filter(b -> min.predecessor == null || (min.predecessor != null && !min.predecessor.equals(b)))
+				.map(b -> new SearchNode(min.current, b, finalMoves))
+				.forEach(s -> {
+					System.out.println(s);
+					pq.insert(s);
+				});
+		} while (true);
 	}
 
 	/***
@@ -14,7 +46,7 @@ public class Solver {
 	 * @return
 	 */
 	public boolean isSolvable() {
-		return false;
+		return true;
 	}
 
 	/**
@@ -23,7 +55,7 @@ public class Solver {
 	 * @return
 	 */
 	public int moves() {
-		return -1;
+		return moves;
 	}
 
 	/**
@@ -32,7 +64,33 @@ public class Solver {
 	 * @return
 	 */
 	public Iterable<Board> solution() {
-		return null;
+		return boards;
+	}
+
+	private static class SearchNode implements Comparable<SearchNode> {
+		private Board predecessor;
+		private Board current;
+		private int moves;
+
+		public SearchNode(Board current) {
+			this(null, current, 0);
+		}
+
+		public SearchNode(Board predecessor, Board current, int moves) {
+			this.predecessor = predecessor;
+			this.current = current;
+			this.moves = moves;
+		}
+
+		@Override
+		public int compareTo(SearchNode other) {
+			return (this.current.manhattan() + this.moves) - (other.current.manhattan() + other.moves);
+		}
+
+		@Override
+		public String toString() {
+			return current.toString();
+		}
 	}
 
 	/**
@@ -41,5 +99,28 @@ public class Solver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String filename = "./8puzzle/puzzle02.txt";
+		// read in the board specified in the filename
+		In in = new In(filename);
+		int n = in.readInt();
+		int[][] tiles = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				tiles[i][j] = in.readInt();
+			}
+		}
+
+		// solve the slider puzzle
+		Board initial = new Board(tiles);
+		Solver solver = new Solver(initial);
+
+		// print solution to standard output
+		if (!solver.isSolvable())
+			StdOut.println("No solution possible");
+		else {
+			StdOut.println("Minimum number of moves = " + solver.moves());
+			for (Board board : solver.solution())
+				StdOut.println(board);
+		}
 	}
 }
